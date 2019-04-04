7b6f3189cf3b58ccf9f2075edab4b19159e13762
Artyom Alekseyevich <32879417+artyomos@users.noreply.github.com>
2019-04-04 09:51:01 -0700

    added all category functions necessary

--- library/category.py
+++ library/category.py
@@ -1,6 +1,21 @@
 import os
 
+# DEBUG for now:
+__file__ = os.getcwd()+'\\simple-file-sort'
+
 def get_categories():
+    dir = os.path.dirname(__file__)
+    for file in os.listdir(dir):
+        if os.path.isfile(file) and file.endswith('.cat'):
+            pass
+
+def make_category(category):
+    pass
+
+def update_category(new_extension, category):
+    pass
+
+def write_categories():
     pass
 
 def choose_category(file):
