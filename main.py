0443d0e0557be9b760d5a05f36a882f486b0be05
Artyom Alekseyevich <32879417+artyomos@users.noreply.github.com>
2019-04-06 10:27:13 -0700

    updated formatting of main and fix for extensions with random capitalization

--- main.py
+++ main.py
@@ -5,31 +5,30 @@ from library import backend
 # Main function - only called if opened
 if __name__ == '__main__':
     while True:
-        path = input('\nWhat is the path for the directory: ').strip()
+        path = input('What is the path for the directory: ').strip()
         if os.path.isdir(path):
             break
         else:
-            print('The path you gave was invalid! Make sure you are typing it properly...')
-    print('\nSorting Files... \nYou may be asked to select a category for each file, you can add your own, or select from already given')
+            print('The path you gave was invalid! Make sure you are typing it properly...\n')
+    print('\nSorting Files... \n\nYou may be asked to select a category for each file, you can add your own, or select from already given')
     print('Please note EVERY FILE will be moved to a folder, so BE CAREFUL HOW YOU USE THIS')
-    input('Press ENTER to continue... or close this window to cancel')
+    input('\nPress ENTER to continue... or close this window to cancel ')
 
     # For now only works on script directory
     dir = path
 
     categories = backend.get_categories()
-    print(dir)
     for file in os.listdir(dir):
-        print(file)
         if os.path.isfile(dir+'\\'+file):
             if '.' in file:
-                extension = file.split('.')[-1]
+                extension = file.split('.')[-1].lower()
                 try:
                     destination_folder = categories[extension]
                 except KeyError:
                     while True:
                         print('\nUh oh! Found an extension (.{}) that isn\'t in any category! \nWhat would you like to do with it?'.format(extension))
                         result = input('Please type only what is shown:\n1. Ignore and leave it\n2. Define a category for it\nChoice: ')
+                        print('')
                         if result == '1':
                             backend.add_extension(categories, 'IGNORE', extension)
                             destination_folder = 'IGNORE'
@@ -45,20 +44,21 @@ if __name__ == '__main__':
                                     print('Ok, going back to previous question')
                                 else:
                                     print('Type what is shown please')
+                                print('')
                                 break
                             break
                         else:
                             print('Very funny. Try again.')
-
             else: # Problem 1: File has no extension
                 destination_folder = 'NO EXTENSION'
 
-            print(file, destination_folder)
+            # Uncomment to debug current working file
+            # print(file, destination_folder)
             if destination_folder == 'IGNORE': pass
             else:
                 print('Moved {} to folder {}'.format(file, destination_folder))
                 os.makedirs(dir+ '\\'+destination_folder, exist_ok=True)
                 shutil.move(dir+ '\\'+file, dir+ '\\'+destination_folder+'\\')
-    print('All file sorting finished. Writing categories..')
+    print('\nAll file sorting finished. Writing categories..')
     backend.write_categories(categories)
     input('Done. Press ENTER to close...')
